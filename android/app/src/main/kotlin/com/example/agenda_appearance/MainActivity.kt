98f13fa168aeda0847b7a1508f657bddc605fed5
Jayaleshwari N <jayaleshwarin@syncfusion.com>
2021-01-08 12:15:08 +0530

    FLUT-3491 - Technical review completed.

---
+++ android/app/src/main/kotlin/com/example/agenda_appearance/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.agenda_appearance
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+import 'package:syncfusion_flutter_calendar/calendar.dart';
+
+void main() => runApp(AgendaAppointmentAppearance());
+
+class AgendaAppointmentAppearance extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+          body: SafeArea(
+            child: SfCalendar(
+              view: CalendarView.month,
+              dataSource: getCalendarDataSource(),
+              monthViewSettings: MonthViewSettings(
+                  showAgenda: true,
+                  agendaStyle: AgendaStyle(
+                    backgroundColor: Colors.tealAccent,
+                    appointmentTextStyle: TextStyle(
+                        fontSize: 14,
+                        fontStyle: FontStyle.italic,
+                        color: Colors.deepPurple),
+                    dateTextStyle: TextStyle(
+                        fontStyle: FontStyle.normal,
+                        fontSize: 12,
+                        fontWeight: FontWeight.w300,
+                        color: Colors.black),
+                    dayTextStyle: TextStyle(
+                        fontStyle: FontStyle.normal,
+                        fontSize: 20,
+                        fontWeight: FontWeight.w700,
+                        color: Colors.black),
+                  )),
+            ),
+          ),
+        ));
+  }
+
+  _DataSource getCalendarDataSource() {
+    final List<Appointment> appointments = <Appointment>[];
+    appointments.add(Appointment(
+      startTime: DateTime.now().add(const Duration(hours: 4, days: -1)),
+      endTime: DateTime.now().add(const Duration(hours: 5, days: -1)),
+      subject: 'Release Meeting',
+      color: Colors.lightBlueAccent,
+    ));
+    appointments.add(Appointment(
+      startTime: DateTime.now().add(const Duration(hours: 2, days: -2)),
+      endTime: DateTime.now().add(const Duration(hours: 4, days: -2)),
+      subject: 'Performance check',
+      color: Colors.amber,
+    ));
+    appointments.add(Appointment(
+      startTime: DateTime.now().add(const Duration(hours: 6, days: -3)),
+      endTime: DateTime.now().add(const Duration(hours: 7, days: -3)),
+      subject: 'Support',
+      color: Colors.green,
+    ));
+    appointments.add(Appointment(
+      startTime: DateTime.now().add(const Duration(hours: 6, days: 2)),
+      endTime: DateTime.now().add(const Duration(hours: 7, days: 2)),
+      subject: 'Retrospective',
+      color: Colors.purple,
+    ));
+
+    return _DataSource(appointments);
+  }
+}
+
+class _DataSource extends CalendarDataSource {
+  _DataSource(List<Appointment> source) {
+    appointments = source;
+  }
+}
