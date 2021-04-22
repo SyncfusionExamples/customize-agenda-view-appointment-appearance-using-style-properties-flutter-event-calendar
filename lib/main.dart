6ddb458063651e8427629a9deac78c355831774d
Indumathi1195R <54974849+Indumathi1195R@users.noreply.github.com>
2021-04-22 08:09:54 +0530

    Update main.dart
    Sample updated.

--- lib/main.dart
+++ lib/main.dart
@@ -1,77 +1,75 @@
-name: agenda_appearance
-description: A new Flutter application.
+import 'package:flutter/material.dart';
+import 'package:syncfusion_flutter_calendar/calendar.dart';
 
-# The following line prevents the package from being accidentally published to
-# pub.dev using `pub publish`. This is preferred for private packages.
-publish_to: 'none' # Remove this line if you wish to publish to pub.dev
+void main() => runApp(AgendaAppointmentAppearance());
 
-# The following defines the version and build number for your application.
-# A version number is three numbers separated by dots, like 1.2.43
-# followed by an optional build number separated by a +.
-# Both the version and the builder number may be overridden in flutter
-# build by specifying --build-name and --build-number, respectively.
-# In Android, build-name is used as versionName while build-number used as versionCode.
-# Read more about Android versioning at https://developer.android.com/studio/publish/versioning
-# In iOS, build-name is used as CFBundleShortVersionString while build-number used as CFBundleVersion.
-# Read more about iOS versioning at
-# https://developer.apple.com/library/archive/documentation/General/Reference/InfoPlistKeyReference/Articles/CoreFoundationKeys.html
-version: 1.0.0+1
+class AgendaAppointmentAppearance extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+          body: SafeArea(
+            child: SfCalendar(
+              view: CalendarView.month,
+              dataSource: getCalendarDataSource(),
+              monthViewSettings: MonthViewSettings(
+                  showAgenda: true,
+                  agendaStyle: AgendaStyle(
+                    backgroundColor: Colors.tealAccent,
+                    appointmentTextStyle: TextStyle(
+                        fontSize: 14,
+                        fontStyle: FontStyle.italic,
+                        color: Colors.deepPurple),
+                    dateTextStyle: TextStyle(
+                        fontStyle: FontStyle.normal,
+                        fontSize: 12,
+                        fontWeight: FontWeight.w300,
+                        color: Colors.black),
+                    dayTextStyle: TextStyle(
+                        fontStyle: FontStyle.normal,
+                        fontSize: 20,
+                        fontWeight: FontWeight.w700,
+                        color: Colors.black),
+                  )),
+            ),
+          ),
+        ));
+  }
 
-environment:
-  sdk: ">=2.12.0 <3.0.0"
+  _DataSource getCalendarDataSource() {
+    final List<Appointment> appointments = <Appointment>[];
+    appointments.add(Appointment(
+      startTime: DateTime.now().add(const Duration(hours: 4, days: -1)),
+      endTime: DateTime.now().add(const Duration(hours: 5, days: -1)),
+      subject: 'Release Meeting',
+      color: Colors.lightBlueAccent,
+    ));
+    appointments.add(Appointment(
+      startTime: DateTime.now().add(const Duration(hours: 2, days: -2)),
+      endTime: DateTime.now().add(const Duration(hours: 4, days: -2)),
+      subject: 'Performance check',
+      color: Colors.amber,
+    ));
+    appointments.add(Appointment(
+      startTime: DateTime.now().add(const Duration(hours: 6, days: -3)),
+      endTime: DateTime.now().add(const Duration(hours: 7, days: -3)),
+      subject: 'Support',
+      color: Colors.green,
+    ));
+    appointments.add(Appointment(
+      startTime: DateTime.now().add(const Duration(hours: 6, days: 2)),
+      endTime: DateTime.now().add(const Duration(hours: 7, days: 2)),
+      subject: 'Retrospective',
+      color: Colors.purple,
+    ));
 
-dependencies:
-  flutter:
-    sdk: flutter
-  syncfusion_flutter_calendar: ^19.1.57
+    return _DataSource(appointments);
+  }
+}
 
-
-  # The following adds the Cupertino Icons font to your application.
-  # Use with the CupertinoIcons class for iOS style icons.
-  cupertino_icons: ^1.0.2
-
-dev_dependencies:
-  flutter_test:
-    sdk: flutter
-
-# For information on the generic Dart part of this file, see the
-# following page: https://dart.dev/tools/pub/pubspec
-
-# The following section is specific to Flutter.
-flutter:
-
-  # The following line ensures that the Material Icons font is
-  # included with your application, so that you can use the icons in
-  # the material Icons class.
-  uses-material-design: true
-
-  # To add assets to your application, add an assets section, like this:
-  # assets:
-  #   - images/a_dot_burr.jpeg
-  #   - images/a_dot_ham.jpeg
-
-  # An image asset can refer to one or more resolution-specific "variants", see
-  # https://flutter.dev/assets-and-images/#resolution-aware.
-
-  # For details regarding adding assets from package dependencies, see
-  # https://flutter.dev/assets-and-images/#from-packages
-
-  # To add custom fonts to your application, add a fonts section here,
-  # in this "flutter" section. Each entry in this list should have a
-  # "family" key with the font family name, and a "fonts" key with a
-  # list giving the asset and other descriptors for the font. For
-  # example:
-  # fonts:
-  #   - family: Schyler
-  #     fonts:
-  #       - asset: fonts/Schyler-Regular.ttf
-  #       - asset: fonts/Schyler-Italic.ttf
-  #         style: italic
-  #   - family: Trajan Pro
-  #     fonts:
-  #       - asset: fonts/TrajanPro.ttf
-  #       - asset: fonts/TrajanPro_Bold.ttf
-  #         weight: 700
-  #
-  # For details regarding fonts from package dependencies,
-  # see https://flutter.dev/custom-fonts/#from-packages
+class _DataSource extends CalendarDataSource {
+  _DataSource(List<Appointment> source) {
+    appointments = source;
+  }
+}
